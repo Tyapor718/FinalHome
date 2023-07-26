e42381bb8a801aa2ae5a32f0b882942f8a353a6e
Tyapor718 <128158397+Tyapor718@users.noreply.github.com>
2023-07-26 22:48:22 +0400

    Programm.cs

--- Desktop/Programm.cs
+++ Desktop/Programm.cs
@@ -2,27 +2,27 @@
 // либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 //  При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
 
-
 void PrintArray(string[] array)
 {
-     for (int i = 0; i < array.Length; i++)
-     {
-         System.Console.Write(array[i] + " ");
-     }
-     System.Console.WriteLine();
- }
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
 void Array(string[] array1, string[] array2)
- {
-     int n = 0;
-     for (int i = 0; i < array1.Length; i++)
-     {
-         if (array1[i].Length < 3)
-         {
-             array2[n] = arrauy[i];
-             n++;
-         }
-     }
- }
- string[] array1 = new string[9] { "art", "start", "first", "1", "low", "234", "type", "of", "classic" };
- string[] array2 = new string[array1.Lenght];
- PrintArray(array2);
+{
+    int n = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[n] = array1[i];
+            n++;
+        }
+    }
+}
+string[] array1 = new string[9] { "art", "start", "first", "1", "low", "234", "type", "of", "classic" };
+string[] array2 = new string[9];
+Array(array1, array2);
+PrintArray(array2);
